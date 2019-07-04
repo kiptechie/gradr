8c3b00a70ae4623ad4f52d78820c8c6547f7fd28
Charles Odili <charles.odili@andela.com>
2019-07-04 15:37:44 +0100

    add firebase performance monitoring

--- src/commons/js/fb-init.js
+++ src/commons/js/fb-init.js
@@ -1,20 +1,23 @@
-import firebase from 'firebase/app';
-import auth from 'firebase/auth';
-import firestore from 'firebase/firestore';
-// import 'firebase/performance';
+import * as firebase from "firebase/app";
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/performance';
 
 const cfg = {
-  apiKey: "AIzaSyAPtzAvpJFH6Dr2MO3TjrgV02dkoBC6Iho",
-  authDomain: "alc-dev-toolkit-d50fe.firebaseapp.com",
-  databaseURL: "https://alc-dev-toolkit-d50fe.firebaseio.com",
-  projectId: "alc-dev-toolkit-d50fe"
+  apiKey: 'AIzaSyAPtzAvpJFH6Dr2MO3TjrgV02dkoBC6Iho',
+  authDomain: 'alc-dev-toolkit-d50fe.firebaseapp.com',
+  databaseURL: 'https://alc-dev-toolkit-d50fe.firebaseio.com',
+  projectId: 'alc-dev-toolkit-d50fe',
+  storageBucket: 'alc-dev-toolkit-d50fe.appspot.com',
+  messagingSenderId: '786236201902',
+  appId: '1:786236201902:web:74e909fd00578ef3'
 };
 
 export const getConfig = () => cfg;
 
 export const init = () => {
   firebase.initializeApp(cfg);
-  // firebase.performance();
+  firebase.performance();
 };
 
 export default { init };
--- src/playground/js/playground.js
+++ src/playground/js/playground.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import * as firebase from "firebase/app";
 
 import marked from 'marked';
 import emmet from '@emmetio/codemirror-plugin';
